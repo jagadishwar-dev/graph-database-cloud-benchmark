63ddeed95bc6765357011554727d3f17778de7fe
Jagadishwar <jagadishwarnakka439@gmail.com>
2026-08-07 18:33:00 +0530

    Use environmental variables for database credentials

--- src/main/java/com/jagadish/benchmark/config/DatabaseConfig.java
+++ src/main/java/com/jagadish/benchmark/config/DatabaseConfig.java
@@ -2,12 +2,7 @@ package com.jagadish.benchmark.config;
 
 public class DatabaseConfig {
 
-    public static final String URI =
-            "bolt+s://db-5a93c6cf.databases.cognodb.com";
-
-    public static final String USERNAME =
-            "cognodb";
-
-    public static final String PASSWORD =
-            "4697dbc6df5d54e4b646289271adeca4";
+    public static final String URI = System.getenv("COGNODB_URI");
+    public static final String USERNAME = System.getenv("COGNODB_USERNAME");
+    public static final String PASSWORD = System.getenv("COGNODB_PASSWORD");
 }
